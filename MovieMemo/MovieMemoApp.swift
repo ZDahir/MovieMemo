70a10e060402d4f3469c0063db83c80202943a79
Zaid Dahir <zaidmdahir@gmail.com>
2023-02-28 00:37:42 -0500

    Initial Commit

---
+++ MovieMemo/MovieMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMemoApp.swift
+//  MovieMemo
+//
+//  Created by Zaid Dahir on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct MovieMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
